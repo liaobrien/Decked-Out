811658f897a3285410fee40893e12447889abf89
Lia OBrien <liaobrien123@gmail.com>
2021-10-20 11:45:18 -0400

    filled out signup.js, login page is rendering

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,10 +32,10 @@ router.get('/', async (req, res) => {
 // login route
 router.get('/login', (req, res) => {
 	// If the user is already logged in, redirect to the homepage
-	if (req.session.loggedIn) {
-		res.redirect('/');
-		return;
-	}
+	// if (!req.session.loggedIn) {
+	// 	res.redirect('/');
+	// 	return;
+	// }
 	// Otherwise, render the 'login' template
 	res.render('login');
 });

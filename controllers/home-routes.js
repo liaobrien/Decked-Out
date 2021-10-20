fafc238a2248a52e26c464917afd63dc918f491e
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-10-20 10:55:41 -0400

    Displays questions on deck and leads you to individual card page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,4 +28,40 @@ router.get('/', async (req, res) => {
 		};
 });
 
+
+router.get('/deck/:id', async (req, res) => {
+	try{
+		const deckbyID = await Deck.findOne({
+			where:{
+					id: req.params.id
+			},
+			attributes:[
+					'id',
+					'name',
+					'user_id'
+				],
+			include: [{
+					model: Card,
+					attributes: ['id', 'question', 'answer', 'deck_id']
+				}]
+			})
+
+			const singleDeck = await deckbyID
+            if (!singleDeck) {
+                res.status(404).json({ message: 'No deck found with this id' });
+                return;
+            }
+
+            const deckCards = singleDeck.get({ plain: true });
+            console.log(deckCards);
+            res.render('deckofCards', { deckCards }); //This is similar to how the homepage works, except if we click on a post it instead renders the individual post!
+
+
+        }
+        catch(err) {
+            console.log(err);
+            res.status(500).json(err);
+        };
+});
+
 module.exports = router;

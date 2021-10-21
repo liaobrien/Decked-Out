1a7ed5c245ad1ff0340932366c33b32dc29d0197
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-10-21 11:45:33 -0400

    Create a new card in a deck

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,12 @@ const userRoutes = require('./user-routes');
 
 const decks = require('./decks');
 
+const newCards = require('./cards')
+
 router.use('/users', userRoutes);
 
 router.use('/decks', decks);
+
+router.use('/cards', newCards)
 
 module.exports = router;
